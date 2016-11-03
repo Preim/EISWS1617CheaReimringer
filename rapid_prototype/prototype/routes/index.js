a203aa16110f70014a4bf308d5c38e613e82be0b
kanonenfutter <van.kanonenfutter@web.de>
2016-11-03 13:38:25 +0100

    send and save json to server.

--- rapid_prototype/prototype/routes/index.js
+++ rapid_prototype/prototype/routes/index.js
@@ -13,23 +13,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/profiles', function(req, res){
-	var id = req.body.id;
-	var speed = req.body.speed;
-	var distance = req.body.distance;
-	profilesCollection.insert(req.body, function(error, profilesCollection){
-		if (error) next(error);
-		else{
-			console.log("success");
-		}
+	// Update. Wenn nicht existent, insert.
+	profilesCollection.update({id: req.body.id}, req.body, {upsert: true}, function(error){
+		if (error) {
+			return console.log('could not update');
+		};
+		console.log('updated. (upsert: true)');
 	});
 
+
+	//matching kriterien berechnen.
+	var matching_sports = req.body.bikesports[0]
+	var location = req.body.location;
+	var speed = req.body.averageSpeed;
+	console.log(matching_sports);
+	profilesCollection.find({bikesports: matching_sports}).toArray(function(error, result){
+		console.log(result);
+
+	});
 	console.log("data: " + JSON.stringify(req.body));
-	//Profil in die Datenbank schreiben
+	
 	//MAtching: Anfrage an datenbank
+
+
+	//Event erstellen und vorschlagen
 
 	res.writeHead(200, 'OK');
 	res.end();
 })
+
+router.get('/profiles/:id', function(req, res){
+	//find ressource 'profile :id'
+});
+router.post('/events', function(req. res){
+	//post data to ressource '/events'
+});
+
+router.get('/notification', function(req, res){
+	
+});
 
 
 
