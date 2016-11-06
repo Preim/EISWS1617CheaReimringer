715156da4dc0d12f473af8a5866694a7320c53a2
kanonenfutter <van.kanonenfutter@web.de>
2016-11-06 22:05:26 +0100

    return matching results. matching criteria edited

--- rapid_prototype/prototype/routes/index.js
+++ rapid_prototype/prototype/routes/index.js
@@ -8,6 +8,17 @@ db.bind('profiles');
 var profilesCollection = db.profiles;
 
 /* GET home page. */
+router.get('/profiles', function (req, res, next) {
+    profilesCollection.findItems(function(error, result){
+        if (error)
+            next(error);
+        else{
+        	console.log(result)
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
+        };
+    });
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -22,31 +33,33 @@ router.post('/profiles', function(req, res){
 	});
 
 
-	//matching kriterien berechnen.
+	//matching kriterien festlegen
+	// Matchingkriterien: Gemeinsame Radsportart, Durchschnitliche Geschwindigkeit ist min. 2km/h bzw. max. 2km/h groesser als medianSpeed
 	var matching_sports = req.body.bikesports[0]
 	var location = req.body.location;
-	var speed = req.body.averageSpeed;
+	var medianSpeed = req.body.averageSpeed;
+	var minSpeed = medianSpeed - 2;
+	if (minSpeed > 0) {
+		minSpeed = 0;
+	};
+	var maxSpeed = medianSpeed + 2;
 	console.log(matching_sports);
-	var results;
-	profilesCollection.find({bikesports: matching_sports}).toArray(function(error, result){
-		console.log(result);
-		results = result;
-
+	var resultsArray;
+	profilesCollection.find({ $and: [{bikesports: matching_sports}, {averageSpeed: {$gte: minSpeed}},{averageSpeed: {$lte: maxSpeed}}]}).toArray(function(error, results){
+    	if (error)
+    		next(error);
+    	else{
+    		console.log(results);
+			resultsArray = results;
+			var jsonObject = {results: resultsArray};
+			res.writeHead(200, {'Content-Type': 'application/json'});
+    		res.end(JSON.stringify(jsonObject));
+    	};
 	});
-	console.log("data: " + JSON.stringify(req.body));
-	
-	//MAtching: Anfrage an datenbank
-
-
-	//Event erstellen und vorschlagen
-
-	//res.writeHead(200, 'OK');
-	res.setHeader('Content-Type','application/json');
-	res.send(JSON.stringify(results));
-	res.end();
 })
 
-router.get('/profiles/:id', function(req, res){
+
+/*router.get('/profiles/:id', function(req, res){
 	//find ressource 'profile :id'
 	profilesCollection.find({id: req.params.id}).toArray(function(error, result){
 		console.log(result);
@@ -55,6 +68,7 @@ router.get('/profiles/:id', function(req, res){
 	res.send(JSON.stringify(result));
 	res.end();
 });
+
 router.post('/events', function(req, res){
 	//post data to ressource '/events'
 	eventsCollection.update({id: req.body.id}, req.body, {upsert: true}, function(error){
@@ -63,12 +77,5 @@ router.post('/events', function(req, res){
 		};
 		console.log('updated. (upsert: true)');
 	});
-});
-
-router.get('/notification', function(req, res){
-
-});
-
-
-
+});*/
 module.exports = router;
