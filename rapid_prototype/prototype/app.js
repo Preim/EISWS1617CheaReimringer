c157310e9e1090e9bbe945e4dbf7424d44b2a451
kanonenfutter <van.kanonenfutter@web.de>
2016-12-09 14:49:35 +0100

    node.js

--- rapid_prototype/prototype/app.js
+++ rapid_prototype/prototype/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var http = require('http');
+
 var mongoDB = require('mongoskin');
 
 var routes = require('./routes/index');
--- rapid_prototype/prototype/routes/index.js
+++ rapid_prototype/prototype/routes/index.js
@@ -7,6 +7,12 @@ db.bind('profiles');
 
 var profilesCollection = db.profiles;
 
+db.bind('events');
+
+var eventsCollection = db.events;
+
+
+
 /* GET home page. */
 router.get('/profiles', function (req, res, next) {
     profilesCollection.findItems(function(error, result){
@@ -58,6 +64,10 @@ router.post('/profiles', function(req, res){
 	});
 })
 
+router.post('/profiles', function(req, res){
+	profilesCollection.
+})
+
 
 /*router.get('/profiles/:id', function(req, res){
 	//find ressource 'profile :id'
@@ -78,4 +88,68 @@ router.post('/events', function(req, res){
 		console.log('updated. (upsert: true)');
 	});
 });*/
+
+router.get('/events', function(req, res, next){
+	eventsCollection.findItems(function(error, result){
+        if (error)
+            next(error);
+        else{
+        	console.log(result)
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
+        };
+    });
+})
+
+router.post('/events', function(req, res){
+	//post data to ressource '/events'
+	eventsCollection.update({id: req.body.id}, req.body, {upsert: true}, function(error){
+		if (error) {
+			return console.log('could not update');
+		};
+		console.log('updated. (upsert: true)');
+	});
+});
+
+router.get('/events/:id', function(req, res){
+	console.log("GET: " + JSON.stringify(req.url));
+    console.log("param: _ID:" + req.params.id);
+    var obj_id = BSON.ObjectID.createFromHexString(req.params.id);
+    eventsCollection.find({_id: obj_id}).toArray(function(error, result) {
+        if (error)
+            next(error);
+        else {
+            console.log('Result:');
+            console.log(result);
+            console.log(result[0]);
+            console.log(result[0].city);
+
+			//TODO: Wetterdatenabfragen
+			var options = {
+  				host: 'api.wunderground.com/',
+  				path: '/api/318295f477098775/forecast10day/lang:DE/q/'+ result[0].city
+			};
+			var str = '';
+			callback = function(response) {
+  				//var str = '';
+
+  				//another chunk of data has been recieved, so append it to `str`
+  				response.on('data', function (chunk) {
+    				str += chunk;
+  				});
+
+  				//the whole response has been recieved, so we just print it out here
+  				response.on('end', function () {
+  					console.log(str);
+  				});
+			}
+
+			http.request(options, callback).end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
+        }
+    });
+})
+
+
 module.exports = router;
