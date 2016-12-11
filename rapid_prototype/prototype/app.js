ba443fa6a6461b2d8c6f0e89d732ce2cb2bd5cf6
Vanakh Chea <van.kanonenfutter@web.de>
2016-12-11 14:54:29 +0100

    retrieve event information and weatherdata

--- rapid_prototype/prototype/app.js
+++ rapid_prototype/prototype/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
-var http = require('http');
+
 
 var mongoDB = require('mongoskin');
 
--- rapid_prototype/prototype/routes/index.js
+++ rapid_prototype/prototype/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongoDB = require('mongoskin');
 var BSON = require('mongodb').BSONPure;
 var db =mongoDB.db('mongodb://localhost/mydb?auto_reconnect=true',{safe: true});
+var http = require('http');
 
 db.bind('profiles');
 
@@ -128,41 +129,69 @@ router.get('/events/:id', function(req, res, error){
 	console.log("GET: " + JSON.stringify(req.url));
     console.log("param: _ID:" + req.params.id);
     //var obj_id = BSON.ObjectID.createFromHexString(req.params.id);
-    eventsCollection.find({_id: {_id: mongoDB.helper.toObjectID(req.params.id)}}).toArray(function(error, result) {
+    eventsCollection.find({_id: mongoDB.helper.toObjectID(req.params.id)}).toArray(function(error, result) {
         if (error){
             next(error);
         }else {
             console.log('Result:');
-            console.log(result);
             console.log(result[0]);
-            console.log(result[0].city);
+            console.log(result[0].start);
+            console.log(result[0].date);
+            var daysUntilEvent = daysfromTodayToEventDate(result[0].date)+1;
+            console.log(daysUntilEvent +  " days until event");
+			if (daysUntilEvent<=10) {
+				//TODO: Wetterdatenabfragen
+				var options = {
+	  				host: 'api.wunderground.com',
+	  				path: '/api/318295f477098775/forecast10day/lang:DE/q/Germany/'+ 'Cologne.json'
+				};
+				var str = '';
+				callback = function(response) {
+	  				//var str = '';
 
-			//TODO: Wetterdatenabfragen
-			var options = {
-  				host: 'api.wunderground.com/',
-  				path: '/api/318295f477098775/forecast10day/lang:DE/q/'+ result[0].city
-			};
-			var str = '';
-			callback = function(response) {
-  				//var str = '';
+	  				//another chunk of data has been recieved, so append it to `str`
+	  				response.on('data', function (chunk) {
+	    				str += chunk;
+	  				});
 
-  				//another chunk of data has been recieved, so append it to `str`
-  				response.on('data', function (chunk) {
-    				str += chunk;
-  				});
+	  				//the whole response has been recieved, so we just print it out here
+	  				response.on('end', function () {
+	  					var weatherdata = JSON.parse(str);
+	  					console.log("High: " + weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].high["celsius"] + 
+	  						" Low: " + weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].low["celsius"] + 
+	  						" Conditions: " + weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].conditions);
+	  					result[0]['high'] = weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].high["celsius"];
+	  					result[0]['low'] = weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].high["celsius"];
+	  					result[0]['conditions'] = weatherdata.forecast.simpleforecast.forecastday[daysUntilEvent].conditions;
+	  				});
+				}
 
-  				//the whole response has been recieved, so we just print it out here
-  				response.on('end', function () {
-  					console.log(str);
-  				});
+				http.request(options, callback).end();
+
 			}
-
-			http.request(options, callback).end();
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(result));
+
         }
     });
 });
 
+function daysBetweenDays(date1, date2){
+	var oneday = 86400000; // 1 Tag in ms
 
+	var date1_ms = date1.getTime();
+	var date2_ms = date2.getTime();
+
+	// Differenz berechnen
+	var diff_ms = date2_ms - date1_ms;
+
+	// in Tagen konvertieren
+	return Math.round(diff_ms/oneday);
+
+}
+function daysfromTodayToEventDate(date){
+	var event_date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+	var today = new Date();
+	return daysBetweenDays(today, event_date);
+}
 module.exports = router;
