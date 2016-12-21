68a19031b489816a7c6ace4c8e8d00b8d9763b61
Vanakh Chea <van.kanonenfutter@web.de>
2016-12-21 19:56:19 +0100

    Navigation Drawer

--- BikeFriends/app/src/main/java/eis/bikefriends/NavigationActivity.java
+++ BikeFriends/app/src/main/java/eis/bikefriends/NavigationActivity.java
@@ -3,6 +3,7 @@ package eis.bikefriends;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -79,18 +80,14 @@ public class NavigationActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.nav_first_layout) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new FirstFragment()).commit();
+        } else if (id == R.id.nav_second_layout) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new SecondFragment()).commit();
+        } else if (id == R.id.nav_third_layout) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new ThirdFragment()).commit();
 
         }
 
