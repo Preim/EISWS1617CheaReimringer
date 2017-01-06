e53482418c53a4bfe5c25cf37b82b8cd5c41c60e
Patrick Reimringer <p.reimringer@hotmail.de>
2017-01-07 00:16:11 +0100

    splashscreen

---
+++ BikeFriends/app/src/main/java/eis/bikefriends/Splashscreen.java
@@ -0,0 +1,26 @@
+package eis.bikefriends;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+    private static int splash_time = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent mainIntent = new Intent(Splashscreen.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, splash_time);
+    }
+
+}
